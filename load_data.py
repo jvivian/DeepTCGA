1b50024a22eb9e3dc536d3c6b69f4f059e60ab4c
Molly <mollyzhang@ucsc.edu>
2017-12-03 01:45:52 -0800

    debug multi-task data loading

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from collections import namedtuple
 from sklearn.preprocessing import LabelBinarizer
 from tensorflow.contrib.learn.python.learn.datasets import base
 from tensorflow.python.framework import dtypes
@@ -7,15 +8,19 @@ from tensorflow.python.framework import dtypes
 
 class DataSet(object):
 
-    def __init__(self, X, y, dtype=dtypes.float32):
-        dtype = dtypes.as_dtype(dtype).base_dtype
-        
-        assert X.shape[0] == y.shape[0]
-        self._num_examples = X.shape[0]
-        self._X = X
-        self._y = y
+    def __init__(self, df):
+        self._num_examples = df.shape[0]
+        self._X = df.iloc[:, 3:].as_matrix()
+        self._y = self.extract_labels(df) 
         self._epochs_completed = 0
         self._index_in_epoch = 0
+
+    def extract_labels(self, df):
+        le = LabelBinarizer()
+        y_tissue = le.fit_transform(df["label_tissue"])
+        y_gender = le.fit_transform(df["label_gender"])
+        y_tumor = le.fit_transform(df["label"])
+        return {"tissue": y_tissue, "gender": y_gender, "tumor": y_tumor}
 
     @property
     def X(self):
@@ -39,51 +44,42 @@ class DataSet(object):
         if self._index_in_epoch > self._num_examples:
             # Finished epoch
             self._epochs_completed += 1
+        
             # Shuffle the data
             perm = np.arange(self._num_examples)
             np.random.shuffle(perm)
             self._X = self._X[perm]
-            self._y = self._y[perm]
+            for key, value in self._y.items():
+                self._y[key] = self._y[key][perm]
             # Start next epoch
             start = 0
             self._index_in_epoch = batch_size
             assert batch_size <= self._num_examples
         end = self._index_in_epoch
-        return self._X[start:end], self._y[start:end]
+        return (self._X[start:end], 
+                self._y["tissue"][start:end],
+                self._y["gender"][start:end],
+                self._y["tumor"][start:end])
 
 
 def read_data_sets(filename, 
-                   label_name, 
-                   dtype=dtypes.float32,                   
-                   test_frac=0.1,
-                   validation_frac=0.1):
+                   test_frac=0.1):
     # read and encode data
     df = pd.read_csv(filename, index_col="sample_id")
-    X = df[df.columns[3:]]
-    X = X.as_matrix()
-    y_raw = df[label_name]
-    le = LabelBinarizer()
-    y = le.fit_transform(y_raw)
-
+    
     # shuffle data
     perm = np.arange(df.shape[0])
     np.random.shuffle(perm)
-    X = X[perm]
-    y = y[perm]
-
-    # split to train, validation and test
+    df = df.iloc[perm, :].copy()
+    # split to train, validddation and test
     test_size = int(test_frac * df.shape[0])
-    validation_size = int(validation_frac * df.shape[0])
-    X_test, y_test = X[:test_size], y[:test_size]
-    X_validation = X[test_size:(test_size+validation_size)]
-    y_validation = y[test_size:(test_size+validation_size)]
-    X_train = X[test_size + validation_size:]
-    y_train = y[test_size + validation_size:]
-
+    df_test = df.iloc[:test_size, :]
+    df_validation = df.iloc[test_size:2*test_size, :]
+    df_train = df.iloc[2*test_size:, :]
+    
     # cast to dataset class
-    train = DataSet(X_train, y_train, dtype=dtype)
-    validation = DataSet(X_validation, y_validation, dtype=dtype)
-    test = DataSet(X_test, y_test, dtype=dtype)
+    train = DataSet(df_train)
+    validation = DataSet(df_validation)
+    test = DataSet(df_test)
 
     return base.Datasets(train=train, validation=validation, test=test)
-
