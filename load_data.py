828b8afbb6a411379d0f61528477469c90330da0
Molly <mollyzhang@ucsc.edu>
2018-03-04 17:15:12 -0800

    very nice refactoring of load data

--- load_data.py
+++ load_data.py
@@ -1,97 +1,68 @@
 import numpy as np
 import pandas as pd
-from collections import namedtuple
-from sklearn.preprocessing import LabelBinarizer, LabelEncoder
-from tensorflow.contrib.learn.python.learn.datasets import base
-from tensorflow.python.framework import dtypes
+import os
+from sklearn.preprocessing import LabelBinarizer
 
 
 class DataSet(object):
-
     def __init__(self, df):
-        self._num_examples = df.shape[0]
-        self._X = df.iloc[:, 3:].as_matrix().astype(np.float32)
-        self._y = self.extract_labels(df) 
-        self._epochs_completed = 0
-        self._index_in_epoch = 0
-
+        self.X = df.iloc[:, 3:].as_matrix().astype(np.float32)
+        self.y = self.extract_labels(df) 
+ 
     def extract_labels(self, df):
         label_dict = {}
         for label_name in ["tissue", "tumor", "gender"]:
             lb = LabelBinarizer()
             label_dict[label_name] = lb.fit_transform(df["label_{0}".format(label_name)])
-
-        # redudant encoding gender label with a column 0 and a column 1 for later speediency
-        reverse_gender = [1-i for i in label_dict["gender"][:, 0]]
-        label_dict["gender"] = np.insert(label_dict["gender"], 0, reverse_gender, axis=1)
+            if label_dict[label_name].shape[1] == 1:
+            # redudantly encode binary labels with two columns for later speediency
+                reverse = [1-i for i in label_dict[label_name][:, 0]]
+                label_dict[label_name] = np.insert(label_dict[label_name], 0, reverse, axis=1)
         return label_dict
 
 
-    @property
-    def X(self):
-        return self._X
+class SplitSet(object):
+    def __init__(self):
+        self.test = None
+        self.cv = []
 
-    @property
-    def y(self):
-        return self._y
-  
-    @property
-    def num_examples(self):
-        return self._num_examples
-  
-    @property
-    def epochs_completed(self):
-        return self._epochs_completed
-  
-    def next_batch(self, batch_size):
-        start = self._index_in_epoch
-        self._index_in_epoch += batch_size
-        if self._index_in_epoch > self._num_examples:
-            # Finished epoch
-            self._epochs_completed += 1
-        
-            # Shuffle the data
-            perm = np.arange(self._num_examples)
-            np.random.shuffle(perm)
-            self._X = self._X[perm]
-            for key, value in self._y.items():
-                self._y[key] = self._y[key][perm]
-            # Start next epoch
-            start = 0
-            self._index_in_epoch = batch_size
-            assert batch_size <= self._num_examples
-        end = self._index_in_epoch
-        batch_x = self._X[start:end]
-        batch_y = {"tissue": self._y["tissue"][start:end],
-                   "gender": self._y["gender"][start:end],
-                   "tumor": self._y["tumor"][start:end]}
-        return batch_x, batch_y
+    def get_first_fold(self):
+        """ use 1st fold of train, validation data by default"""
+        self.train = self.cv[0][0]
+        self.val = self.cv[0][0]
 
 
-    def reset_epoch(self):
-        self._epochs_completed = 0
-
-
-def read_data_sets(filename, test_frac=0.1):
-    # read and encode data
+def read_data_sets(filename, random_state=0):
+    data = SplitSet() 
+    data_path = "./data/train_val_test_split/seed{0}".format(random_state)
+    # read data
     df_X = pd.read_csv(filename, index_col="sample_id")
     df_y = pd.read_csv("./data/labels.csv", index_col="sample_id")
     df = df_y.join(df_X).dropna()
 
-    # shuffle data
-    perm = np.arange(df.shape[0])
-    np.random.shuffle(perm)
-    df = df.iloc[perm, :]
+    # seperate to train and val from test
+    test_file = os.path.join(data_path, "test.csv")
+    df_test = df.loc[get_sample_ids(test_file)]
+    data.test = DataSet(df_test) 
 
-    # split to train, validddation and test
-    test_size = int(test_frac * df.shape[0])
-    df_test = df.iloc[:test_size, :]
-    df_validation = df.iloc[test_size:2*test_size, :]
-    df_train = df.iloc[2*test_size:, :]
+    # seperate train from val in cross validation
+    cv_samples = {}
+    for fold in range(5):
+        train_file = os.path.join(data_path, "train_fold{0}.csv".format(fold))
+        val_file = os.path.join(data_path, "val_fold{0}.csv".format(fold))
+        df_train = df.loc[get_sample_ids(train_file)]
+        df_val = df.loc[get_sample_ids(val_file)]
+        data.cv.append((DataSet(df_train), DataSet(df_val)))
     
-    # cast to dataset class
-    train = DataSet(df_train)
-    validation = DataSet(df_validation)
-    test = DataSet(df_test)
+    data.get_first_fold()    
+    return data
 
-    return base.Datasets(train=train, validation=validation, test=test)
+
+def get_sample_ids(filename):
+    with open(filename, "r") as f:
+        return f.read().strip("\n").split("\n")
+  
+
+ 
+
+
